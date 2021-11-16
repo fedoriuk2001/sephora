7447acb6da89b499b73d6b995a106f8bf5d18af1
fedoriuk2001 <fedoriuk2001@gmail.com>
2021-11-16 18:32:50 +0200

    Second Part

--- js/app.js
+++ js/app.js
@@ -1,32 +1,93 @@
-$(".selected-site").click(function(){
-    $(".ui-dialog").toggle();
-    $(".ui-widget-overlay").toggle(); 
-    window.scrollTo(0, 0);
-});
+$(document).ready(function() {
+    $(".selected-site").on("click", function(){
+        $(".ui-dialog").toggle();
+        $(".ui-widget-overlay").toggle(); 
+        window.scrollTo(0, 0);
+    });
 
-$(".ui-dialog-titlebar-close").click(function(){
-    $(".ui-dialog").toggle();
-    $(".ui-widget-overlay").toggle(); 
-});
+    $(".ui-dialog-titlebar-close").on("click", function(){
+        $(".ui-dialog").toggle();
+        $(".ui-widget-overlay").toggle(); 
+        $(".refinement-menu").css({ "display": "none" }); 
+    });
 
-$(".accordion-title").click(function(){
-    $(".accordion-content").toggle();
-    $(".accordion-item").addClass('is-active'); 
-    return false;
-});
+    $(".accordion-title").on("click" ,function(){
+        $(".accordion-content").toggle();
+        $(".accordion-item").addClass('is-active'); 
+        return false;
+    });
 
-$(".filter-by-btn").click(function(){
-    $(".refinements-mobile").toggle(); 
-});
+    $(".filter-by-btn").on("click", function(){
+        $(".refinements-mobile").toggle(); 
+    });
 
-$(".close-icon").click(function(){
-    $(".refinements-mobile").toggle(); 
-});
+    $(".close-icon").on("click", function(){
+        $(".refinements-mobile").toggle(); 
+        $(".refinement-menu").css({ "display": "none" }); 
+    });
 
-$('.open-close-main').click(function(){
-    if($('.opened-closed-element').hasClass('refinements-open')){
-        $(".opened-closed-element").removeClass('refinements-open');
-      }else{
-        $(".opened-closed-element").addClass('refinements-open');
-      }
-});
+    $('.open-close-main').on("click", function(){
+        if($('.opened-closed-element').hasClass('refinements-open')){
+            $(".opened-closed-element").removeClass('refinements-open');
+          }else{
+            $(".opened-closed-element").addClass('refinements-open');
+          }
+    });
+
+    $('.header-search').on("click", function(){
+        if($('.search-popin-overlay').hasClass('active')){
+            $(".search-popin-overlay").removeClass('active');
+          }else{
+            $(".search-popin-overlay").addClass('active');
+          }
+    });
+    
+    $('.header-search').on("click", function(){
+        if($('.search-popin').hasClass('active')){
+            $(".search-popin").removeClass('active');
+          }else{
+            $(".search-popin").addClass('active');
+          }
+    });
+    
+    $('.close-search-popin').on("click", function(){
+        if($('.search-popin').hasClass('active')){
+            $(".search-popin").removeClass('active');
+          }else{
+            $(".search-popin").removeClass('active');
+          }
+    });
+    
+    $('.close-search-popin').on("click", function(){
+        if($('.search-popin-overlay').hasClass('active')){
+            $(".search-popin-overlay").removeClass('active');
+          }else{
+            $(".search-popin-overlay").removeClass('active');
+          }
+    });
+    
+    $(window).scroll(
+        {
+            previousTop: 0
+        }, 
+        function () {
+        var currentTop = $(window).scrollTop();
+        if (currentTop < this.previousTop) {
+            $("#navigation").addClass('slideInDown');
+            $("#navigation").addClass('sticky-top-nav');
+            $("#navigation").removeClass('slideOutUp');
+            $(".desktop-header").addClass('slideInDown');
+            $(".desktop-header").addClass('sticky-top');
+            $(".desktop-header").removeClass('slideOutUp');
+        } else {
+            $("#navigation").addClass('slideOutUp');
+            $("#navigation").removeClass('sticky-top-nav');
+            $("#navigation").removeClass('slideInDown');
+            $(".desktop-header").addClass('slideOutUp');
+            $(".desktop-header").removeClass('sticky-top');
+            $(".desktop-header").removeClass('slideInDown');
+        }
+        this.previousTop = currentTop;
+    });
+
+})
